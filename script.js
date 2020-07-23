a9db6c6e01a4a00b3c400f49bc68dc56232e4341
Kayla Zoschg <kaylazoschg@Kaylas-MacBook-Pro-2.local>
2020-07-23 14:11:40 -0400

    finished assignment and bonus mission

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.addEventListener('load', function(){
       let fuelStatus = document.getElementById('fuelStatus');
       let launchStatus = document.querySelector('h2');
       let cargoStatus = document.getElementById('cargoStatus');
+      const missionTarget = document.getElementById('missionTarget');
 
       //validation alerts
       if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || cargoInput.value === "") {
@@ -33,36 +34,52 @@ window.addEventListener('load', function(){
       }
 
       //updating shuttle requirements
-      pilotStatus.innerHTML = `${pilotInput.value} is ready for launch.`;
-      copilotStatus.innerHTML = `${copilotInput.value} is ready for launch.`;
+      pilotStatus.innerHTML = `Pilot ${pilotInput.value} is ready for launch`;
+      copilotStatus.innerHTML = `Co-pilot ${copilotInput.value} is ready for launch`;
       if (fuelInput.value<10000) {
          faultyItems.style.visibility = 'visible';
-         fuelStatus.innerHTML = "There is not enough fuel for the journey.";
-         launchStatus.innerHTML = 'Shuttle not ready for launch.';
+         fuelStatus.innerHTML = "There is not enough fuel for the journey";
+         launchStatus.innerHTML = 'Shuttle not ready for launch';
          launchStatus.style.color = 'red';
       } else if (cargoInput.value>10000) {
          faultyItems.style.visibility = 'visible';
-         cargoStatus.innerHTML = 'There is too much mass for the shuttle to take off.';
-         launchStatus.innerHTML = 'Shuttle not ready for launch.';
+         cargoStatus.innerHTML = 'There is too much mass for the shuttle to take off';
+         launchStatus.innerHTML = 'Shuttle not ready for launch';
          launchStatus.style.color = 'red';
       } else {
          faultyItems.style.visibility = 'visible';
-         launchStatus.innerHTML = 'Shuttle is ready for launch.';
+         launchStatus.innerHTML = 'Shuttle is ready for launch';
          launchStatus.style.color = 'green';
       }
+   });
 
-      //fetching planetary data
+   //fetching planetary data
+   fetch('https://handlers.education.launchcode.org/static/planets.json').then(function(response){
+      response.json().then(function(json){
+         let randomPlanet = Math.floor(Math.random()*json.length);
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[randomPlanet].name}</li>
+            <li>Diameter: ${json[randomPlanet].diameter}</li>
+            <li>Star: ${json[randomPlanet].star}</li>
+            <li>Distance from Earth: ${json[randomPlanet].distance}</li>
+            <li>Number of Moons: ${json[randomPlanet].moons}</li>
+         </ol>
+         <img src="${json[randomPlanet].image}">
+         `;
+      });
    });
 });
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
+   <li>Name: ${json[0].name}</li>
+   <li>Diameter: ${json[0].diameter}</li>
+   <li>Star: ${json[0].star}</li>
+   <li>Distance from Earth: ${json[0].distance}</li>
+   <li>Number of Moons: ${json[0].moons}</li>
 </ol>
-<img src="${}">
+<img src="${json[0].image}">
 */
